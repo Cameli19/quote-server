7c785d7bfa4084101d6d08461f83f0f4dbd27da6
Camellia Tran <cametran@cecs.pdx.edu>
2025-04-23 22:00:36 -0700

    Third commit

--- quote-server/src/template.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-
-use askama::Template;
-
-#[derive(Template)]
-#[Template(path = "index.html")]
-pub struct IndexTemplate<'a> {
-    pub quote: &'a Quote,
-}
---
+++ quote-server/src/templates.rs
@@ -0,0 +1,19 @@
+use crate::*;
+
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+pub struct IndexTemplate<'a> {
+    pub quote: &'a Quote,
+    stylesheet: &'static str,
+}
+
+impl<'a> IndexTemplate<'a> {
+    pub fn quote(quote: &'a Quote) -> Self {
+        Self {
+            quote,
+            stylesheet: "/quote.css"
+        }
+    }
+}
