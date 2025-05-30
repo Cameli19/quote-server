98de7346e28f8ef95c4067e4b05474e227f589d1
Camellia Tran <cametran@cecs.pdx.edu>
2025-05-30 16:19:58 -0700

    Updated quote.rs

--- quote-server/src/quote.rs
+++ quote-server/src/quote.rs
@@ -1,9 +1,108 @@
+use crate::*;
+
+use std::collections::HashSet;
+use std::ops::Deref;
+use std::path::Path;
+
+use crate::QuoteError;
+
+use serde::Deserialize;
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct Quote {
-    pub text: &'static str,
-    pub author: &'static str,
+    id: String,
+    text: String, 
+    author: String,
+    tags: HashSet<String>,
+    source: String,
 }
 
-pub const THE_QUOTE: Quote = Quote {
-    text: "Without effort, your talent is nothing more than unmet potential",
-    author: "Angela Duckworth",
-};
+#[derive(Clone)]
+pub struct Quote {
+    pub id: String,
+    pub text: String,
+    pub author: String,
+    pub source: String,
+}
+
+pub fn read_quotes<P: AsRef<Path>>(quotes_path: P) -> Result<Vec<JsonQuote>, QuoteError> {
+    let f = std::fs::File::open(quotes_path.as_ref())?;
+    let quotes = serde_json::from_reader(f)?;
+    Ok(quotes)
+}
+
+impl JsonQuote {
+    pub fn new(quote: Quote, tags: Vec<String>) -> Self {
+        let tags = tags.into_iter().collect();
+        Self {
+            id: quote.id,
+            text: quote.text,
+            author: quote.author,
+            tags,
+            source: quote.source,
+        }
+    }
+
+    pub fn to_quote(&self) -> (Quote, impl Iterator<Item = &str>) {
+        let quote = Quote {
+            id: self.id.clone(),
+            text: self.text.clone(),
+            author: self.author.clone(),
+            source: self.source.clone(),
+        };
+        let tags = self.tags.iter().map(String::deref);
+        (quote, tags)
+    }
+}
+
+impl axum::response::IntoResponse for &JsonQuote {
+    fn into_response(self) -> axum::response::Response {
+        (http::StatusCode::OK, axum::Json(&self)).into_response()
+    }
+}
+
+pub async fn get(db: &SqlitePool, quote_id: &str) -> Result<(Quote, Vec<String>), sqlx::Error> {
+    let quote = sqlx::query_as!(Quote, "SELECT * FROM jokes WHERE id = $1;", quote_id)
+        .fetch_one(db)
+        .await?;
+
+    let tags: Vec<String> = sqlx::query_scalar!("SELECT tag FROM tags WHERE quote_id = $1;", quote_id)
+        .fetch_all(db)
+        .await?;
+
+    Ok((quote, tags))
+}
+
+pub async fn get_tagged<'a, I>(db: &SqlitePool, tags: I) -> Result<Option<String>, sqlx::Error>
+    where I: Iterator<Item=&'a str>
+{
+    let mut qtx = db.begin().await?;
+    sqlx::query("DROP TABLE IF EXISTS qtags;").execute(&mut *qtx).await?;
+    sqlx::query("CREATE TEMPORARY TABLE qtags (tag VARCHR(200));")
+        .execute(&mut *qtx)
+        .await?;
+    for tag in tags {
+        sqlx::query("INSERT INTO qtags VALUES ($1);")
+            .bind(tag)
+            .execute(&mut *qtx)
+            .await?;
+    }
+    let quote_ids = sqlx::query("SELECT DISTINCT quote_id FROM tags JOIN qtags ON quote_tags.tag = qtags.tag ORDER BY RANDOM() LIMIT 1;")
+        .fetch_all(&mut *qtx)
+        .await?;
+    let nquote_ids = quote_ids.len();
+    let result = if nquote_ids == 1 {
+        Some(joke_ids[0].get(0))
+    } else {
+        None
+    };
+    qtx.commit().await?;
+
+    Ok(result)
+}
+
+pub async fn get_random(db: &SqlitePool) -> Result<String, sqlx::Error> {
+    sqlx::query_scalar!("SELECT id FROM quotes ORDER BY RANDOM() LIMIT 1;")
+        .fetch_one(db)
+        .await
+}
