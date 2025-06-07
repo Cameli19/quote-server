aa333c83d4cf7941f2416eab1478d5959073aa47
Camellia Tran <cametran@cecs.pdx.edu>
2025-06-06 20:57:36 -0700

    Added quote-client

---
+++ quote-client/src/quote.rs
@@ -0,0 +1,28 @@
+use leptos::prelude::*;
+use serde::{Serialize, Deserialize};
+use std::collections::HashSet;
+
+#[derive(Serialize, Deserialize)]
+pub struct Joke {
+    pub id: String,
+    pub content: String, 
+    pub author: String,
+    pub tags: HashSet<String>,
+    pub source: String,
+}
+
+
+pub async fn fetch(endpoint: String) -> Result<Quote, Error> {
+    use reqwasm::http::Request;
+
+    let ep = format!(
+        "http://localhost:3000/api/v1/{}",
+        endpoint,
+    );
+    let result = Request::get(&ep)
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(result)
+}
